30f9b28a4ca8de6d2f4d1193bb9ac80f10c615f8
piteredo <piteredo@hotmail.com>
2019-06-30 11:33:30 +0900

    css

--- index.php
+++ index.php
@@ -5,17 +5,43 @@ $_GET['robot'] = 'index';
 $_GET['page_name'] = $page_name;
 require($root.'header.php');
 
+//UPDATES
+$updates = [];
+
+//BIO
+$bio_data = getPDOStatement($pdo, BIOGRAPHY_SQL)->fetch(); //only 1 column
+$photos = getPDOStatement($pdo, PHOTO_SQL)->fetchAll();
+$type = 'biography';
+$date = getUpdateDate($pdo, 'biography');
+$fullpath = $root.'img/bio/'.$photos[0]['src'].'.jpg';
+$title = $bio_data['janame'];
+$text = $bio_data['jatext'];
+$updates[] = array('type'=>$type, 'date'=>$date, 'fullpath'=>$fullpath, 'title'=>$title, 'text'=>$text);
+
+//SCHEDULE
+$schedule_data = getPDOStatement($pdo, SCHEDULE_SQL)->fetchAll();
+$schedule_data = scheduleDateSortAsc($schedule_data, "date");
+$type = 'schedule';
+$date = getUpdateDate($pdo, 'schedule');
+if($schedule_data[0]['imgurl'] != null) $fullpath = $root. 'img/design/'. $schedule_data[0]['imgurl'];
+else $fullpath = $root. 'img/no-image.png';
+$title = $schedule_data[0]['date'] . ' ' . $schedule_data[0]['title'];
+$text = "###";
+$updates[] = array('type'=>$type, 'date'=>$date, 'fullpath'=>$fullpath, 'title'=>$title, 'text'=>$text);
+
 //VIDEO
 $maxResults = 2;
 $address = $apiAddress . $checkId . $fragment . "&key=" . $yt_apiKey . "&maxResults=" . $maxResults;
 $json = cuGet_contents( $address );
 $json_decode = json_decode($json, true);
-$dates = [];
 foreach ($json_decode['items'] as $video) {
-  $dates[] = substr($video['snippet']['publishedAt'], 0, 10);
+  $type = 'video';
+  $date = substr($video['snippet']['publishedAt'], 0, 10);
+  $fullpath = 'https://www.youtube.com/embed/' . $video['snippet']['resourceId']['videoId'] . '?rel=0';
+  $title = "#";
+  $text = "###";
+  $updates[] = array('type'=>$type, 'date'=>$date, 'fullpath'=>$fullpath, 'title'=>$title, 'text'=>$text);
 }
-rsort($dates, SORT_NUMERIC);
-$video_update_date = $dates[0];
 
 //DESIGN
 $num   = 3;
@@ -23,32 +49,61 @@ $query = 'media.limit('. $num. '){caption,media_url,permalink,timestamp}';
 $json  = file_get_contents("{$f_api}{$ig_id}?fields={$query}&access_token={$token}");
 $data  = json_decode($json, true);
 $medias = $data['media']['data'];
-$design_update_date = substr($medias[0]['timestamp'], 0, 10);
+foreach ($medias as $media) {
+  $type = 'design';
+  $date = substr($media['timestamp'], 0, 10);
+  $fullpath = $media['media_url'];
+  $title = "#";
+  $text = deleteHashTags($media['caption']);
+  $updates[] = array('type'=>$type, 'date'=>$date, 'fullpath'=>$fullpath, 'title'=>$title, 'text'=>$text);
+}
+
+//DISCO
+$discography_data = getPDOStatement($pdo, DISCOGRAPHY_SQL)->fetchAll();
+$type = 'discography';
+$date = getUpdateDate($pdo, 'discography');
+$fullpath = $root. 'img/design/'. $discography_data[0]['imgurl']. '.jpg';
+$title = $discography_data[0]['title'];
+$text = "###";
+$updates[] = array('type'=>$type, 'date'=>$date, 'fullpath'=>$fullpath, 'title'=>$title, 'text'=>$text);
+
+
+$updates = dateSort($updates);
+$total_update_date = $updates[0]['date'];
+
+//echo json_encode($updates);
 ?>
 
 <main class="main">
   <section class="content">
-    <h2 class="content__header-title">
+    <!--<h2 class="content__header-title">
       <?=INDEX_GREETING_TITLE?>
     </h2>
     <p class="content__header-update-date">
-      <?=SYNC_ICON?><time><?=$video_update_date?></time>
-    </p>
+      <?=SYNC_ICON?><time><?=$total_update_date?></time>
+    </p>-->
     <p class="content__description">
       <?=INDEX_GREETING_TEXT?>
     </p>
   </section>
 
   <section class="content">
-    <h2 class="content__header-title"><?=VIDEO_EN?></h2>
-    <p class="content__header-update-date"><?=SYNC_ICON?><time><?=$video_update_date?></time></p>
+    <h2 class="content__header-title"><?=UPDATES_EN?></h2>
+    <p class="content__header-update-date"><?=SYNC_ICON?><time><?=$total_update_date?></time></p>
     <ul>
-    <?php foreach($json_decode['items'] as $video):
-      $videoId = $video['snippet']['resourceId']['videoId'];
+    <?php
+    foreach($updates as $content):
+      if($content['type'] == "video"):
       ?>
-      <li>
+      <li class="section">
+        <h3 class="section__title-text">
+          <?=$content['title']?>
+        </h3>
+        <p class="section__label">
+          <?=$content['text']?>
+        </p>
         <iframe
-          src = "https://www.youtube.com/embed/<?=$videoId?>?rel=0"
+          src = <?=$content['fullpath']?>
           width = "560"
           height = "315"
           frameborder = "0"
@@ -56,25 +111,19 @@ $design_update_date = substr($medias[0]['timestamp'], 0, 10);
           allowfullscreen>
         </iframe>
       </li>
-    <?php endforeach; ?>
-    </ul>
-  </section>
-
-  <section class="content">
-    <h2 class="content__header-title"><?=DESIGN_EN?></h2>
-    <p class="content__header-update-date"><?=SYNC_ICON?><time><?=$design_update_date?></time></p>
-    <ul>
-    <?php foreach($medias as $media):
-      $permalink = $media['permalink'];
-      $media_url = $media['media_url'];
-      $caption = deleteHashTags($media['caption']);
-      ?>
-      <li>
-        <a href="<?=$permalink?>">
-          <img src="<?=$media_url?>" alt="<?=$caption?>">
+    <?php else: ?>
+      <li class="section">
+        <h3 class="section__title-text">
+          <?=$content['title']?>
+        </h3>
+        <p class="section__label">
+          <?=$content['text']?>
+        </p>
+        <a href="<?=$content['fullpath']?>">
+          <img src="<?=$content['fullpath']?>" alt="<?=$content['text']?>">
         </a>
       </li>
-    <?php endforeach; ?>
+    <?php endif; endforeach; ?>
     </ul>
   </section>
 </main>
